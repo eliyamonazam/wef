d85f19b9a70d839ed311f1a8d006d9c93e330ccb
nolimiya <83608686+eliyamonazam@users.noreply.github.com>
2025-04-03 16:23:14 +0330

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update the system
+sudo apt-get update
+
+# Install necessary dependencies
+sudo apt-get install g++ make
+
+# Clone the repository (if not already cloned)
+git clone https://github.com/nolimiya/sudoku-game.git
+cd sudoku-game
+
+# Compile the code
+make
+
+# Run the game
+./sudoku_game
